774495ee0c00024b055464cd3fabacd30bd0a37b
nymhyde <heydhruvpatel@gmail.com>
2022-09-02 21:03:15 +0530

    update

--- fractal.py
+++ fractal.py
@@ -9,7 +9,10 @@ import time
 
 
 
-img_size = int(input('Enter Image Size : '))
+width = int(input('Enter Width for Image : '))
+height = int(input('Enter Height for Image : '))
+
+print(f'Total Number of Pixels :: {width*height}')
 
 # << Pure Python Run >> #
 
@@ -39,11 +42,14 @@ def create_fractal(min_x, max_x, min_y, max_y, image, iters):
             color = mandel(real, imag, iters)
             image[y,x] = color
 
+def pure_python():
+    create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
 
-image = np.zeros((img_size, img_size), dtype=np.uint8)
+
+image = np.zeros((height, width), dtype=np.uint8)
 
 start = time.time()
-create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
+pure_python()
 end = time.time()
 py_run = end - start
 print(f'Pure Python Run :: Total Elapsed Time to run the program : {py_run:.2f} seconds')
@@ -52,7 +58,7 @@ print(f'Pure Python Run :: Total Elapsed Time to run the program : {py_run:.2f} 
 # << Just-In-Time Compiler >> #
 
 @jit(nopython=True)
-def mandel(x,y, max_iters):
+def jit_mandel(x,y, max_iters):
     c = complex(x, y)
     z = 0.0j
 
@@ -64,7 +70,7 @@ def mandel(x,y, max_iters):
     return max_iters
 
 @jit(nopython=True)
-def create_fractal(min_x, max_x, min_y, max_y, image, iters):
+def jit_create_fractal(min_x, max_x, min_y, max_y, image, iters):
     height = image.shape[0]
     width = image.shape[1]
 
@@ -76,29 +82,29 @@ def create_fractal(min_x, max_x, min_y, max_y, image, iters):
 
         for y in range(height):
             imag = min_y + y * pixel_size_y
-            color = mandel(real, imag, iters)
+            color = jit_mandel(real, imag, iters)
             image[y,x] = color
 
-image = np.zeros((img_size, img_size), dtype=np.uint8)
+image = np.zeros((height, width), dtype=np.uint8)
 
 # < First JIT run : where it compiles the decorated function into machine code >
 start = time.time()
-create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
+jit_create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
 end = time.time()
 just_compile = end - start
 print(f'First JIT run :: Total Elapsed Time to run the program : {just_compile:.2f} seconds')
 
 # < Pre-compile run : here the decorated functions are already converted to machine code >
 start = time.time()
-create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
+jit_create_fractal(-2.0, -1.7, -0.1, 0.1, image, 20)
 end = time.time()
 pre_compile = end - start
 print(f'Pre-Compiled JIT run :: Total Elapsed Time to run the program : {pre_compile:.2f} seconds')
 
 
-print(f'\n\nFirst JIT is faster than Pure Python run by a factor of : {py_run/just_compile}')
-print(f'Pre-Compiled JIT is faster than Pure Python run by a factor of : {py_run/pre_compile}')
-print(f'Pre-Compiled JIT is faster than first JIT run by a factor of : {just_compile/pre_compile}')
+print(f'\n\nNon-Compiled JIT over Pure Python by a factor of : {py_run/just_compile}')
+print(f'Pre-Compiled JIT over Pure Python by a factor of : {py_run/pre_compile}')
+print(f'Pre-Compiled JIT over Non-Compiled JIT by a factor of : {just_compile/pre_compile}')
 
 
 # << Plotting Fractal Image >> #
